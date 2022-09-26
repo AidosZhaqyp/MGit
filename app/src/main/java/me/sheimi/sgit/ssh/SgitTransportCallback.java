07ecb75d1d02ace34c521fd30ba4c80a6806ca4d
Pranav <purwarpranav80@gmail.com>
2022-09-26 23:44:28 +0530

    Update SgitTransportCallback.java

--- app/src/main/java/me/sheimi/sgit/ssh/SgitTransportCallback.java
+++ app/src/main/java/me/sheimi/sgit/ssh/SgitTransportCallback.java
@@ -3,6 +3,7 @@ package me.sheimi.sgit.ssh;
 import org.eclipse.jgit.api.TransportConfigCallback;
 import org.eclipse.jgit.transport.SshTransport;
 import org.eclipse.jgit.transport.Transport;
+import org.eclipse.jgit.transport.SshSessionFactory;
 
 /**
  * Created by sheimi on 8/22/13.
@@ -11,14 +12,10 @@ public class SgitTransportCallback implements TransportConfigCallback {
 
     private SGitSessionFactory ssh;
 
-    public SgitTransportCallback() {
-        ssh = new SGitSessionFactory();
-    }
-
     @Override
     public void configure(Transport tn) {
         if (tn instanceof SshTransport) {
-            ((SshTransport) tn).setSshSessionFactory(ssh);
+            ((SshTransport) tn).setSshSessionFactory(SshSessionFactory.getInstance());
         }
     }
 }
